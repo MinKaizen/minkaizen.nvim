13cfc16ff5bda5319a2ff22f2f8508f3df2c69e7
minkaizen <martin.g.cao@gmail.com>
2025-05-16 20:18:34 +1000

    install mini.jump

--- init.lua
+++ init.lua
@@ -1075,6 +1075,8 @@ require('lazy').setup({
 
       require('mini.move').setup()
 
+      require('mini.jump').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
