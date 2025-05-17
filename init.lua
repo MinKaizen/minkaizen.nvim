34c19a15a1925cb5bfef4b3db0024cb0a0a2dbc8
minkaizen <martin.g.cao@gmail.com>
2025-05-18 06:31:11 +1000

    add command: DapCloseAll

--- init.lua
+++ init.lua
@@ -254,6 +254,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_user_command('DapCloseAll', function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local bufname = vim.api.nvim_buf_get_name(buf)
+    if bufname:match 'dap%-repl' or bufname:match 'DAP' then
+      vim.api.nvim_win_close(win, true)
+    end
+  end
+end, {})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
