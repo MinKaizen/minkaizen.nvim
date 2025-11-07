9bcd6b615394a142fb0e34300411d303808ff526
minkaizen <martin.g.cao@gmail.com>
2025-11-07 16:07:10 +1100

    feat: disable better escape in visual mode

--- lua/custom/plugins/better_escape.lua
+++ lua/custom/plugins/better_escape.lua
@@ -1,6 +1,18 @@
 return {
   'max397574/better-escape.nvim',
   config = function()
-    require('better_escape').setup()
+    require('better_escape').setup {
+      -- Only enable in insert mode, not visual mode
+      default_mappings = true,
+      mappings = {
+        i = {
+          j = {
+            k = '<Esc>',
+            j = '<Esc>',
+          },
+        },
+        v = {}, -- Disable all mappings in visual mode
+      },
+    }
   end,
 }
