2d62daba37a3f1740e6f0ac75c730fdc7095204c
minkaizen <martin.g.cao@gmail.com>
2025-11-17 20:14:50 +1100

    feat: bump syntax

--- init.lua
+++ init.lua
@@ -869,9 +869,9 @@ require('lazy').setup({
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require('lspconfig').gdscript.setup {}
-      require('lspconfig').intelephense.setup {}
-      require('lspconfig').emmet_language_server.setup {
+      vim.lsp.config('gdscript', {})
+      vim.lsp.config('intelephense', {})
+      vim.lsp.config('emmet_language_server', {
         filetypes = { 'css', 'eruby', 'html', 'javascript', 'javascriptreact', 'less', 'sass', 'scss', 'pug', 'typescriptreact', 'blade', 'liquid' },
         -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
         -- **Note:** only the options listed in the table are supported.
@@ -895,7 +895,7 @@ require('lazy').setup({
           --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
           variables = {},
         },
-      }
+      })
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -964,7 +964,7 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            vim.lsp.config[server_name].setup(server)
           end,
         },
       }
