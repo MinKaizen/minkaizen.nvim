9b51de1c0f43c0df805a0746419efdc8cfb82375
Martin <martin.cao@hustlrmedia.com>
2026-03-14 18:03:38 +1100

    feat: add zk keybinding

--- lua/custom/plugins/zk.lua
+++ lua/custom/plugins/zk.lua
@@ -1,35 +1,44 @@
 -- Define the function, but DO NOT call it yet
 local config_func = function()
-  local notebook_dir = os.getenv("ZK_NOTEBOOK_DIR")
+  local notebook_dir = os.getenv 'ZK_NOTEBOOK_DIR'
 
   if not notebook_dir then
-    vim.notify(
-      "ZK_NOTEBOOK_DIR is not set! zk-nvim is falling back to the current directory.",
-      vim.log.levels.WARN,
-      { title = "Zk-Nvim Configuration" }
-    )
+    vim.notify('ZK_NOTEBOOK_DIR is not set! zk-nvim is falling back to the current directory.', vim.log.levels.WARN, { title = 'Zk-Nvim Configuration' })
     notebook_dir = vim.fn.getcwd()
   end
 
-  require("zk").setup({
+  require('zk').setup {
     notebook_path = notebook_dir,
-    picker = "telescope",
+    picker = 'telescope',
     lsp = {
       config = {
-        cmd = { "zk", "lsp" },
-        name = "zk",
+        cmd = { 'zk', 'lsp' },
+        name = 'zk',
       },
       auto_attach = {
         enabled = true,
-        filetypes = { "markdown" },
+        filetypes = { 'markdown' },
       },
     },
+  }
+
+  local function set_keymaps(bufnr)
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('v', '<leader>zn', ":ZkNewFromTitleSelection<CR>", opts)
+    vim.keymap.set('n', 'gr', '<Cmd>ZkBacklinks<CR>', opts)
+  end
+
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'markdown',
+    callback = function(args)
+      set_keymaps(args.buf)
+    end,
   })
 end
 
--- Return the TABLE lazy expects. 
+-- Return the TABLE lazy expects.
 -- Note that we pass the function to the 'config' key WITHOUT ()
 return {
-  "zk-org/zk-nvim",
-  config = config_func
+  'zk-org/zk-nvim',
+  config = config_func,
 }
